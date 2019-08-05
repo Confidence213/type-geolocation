336a5ae97317870b5da4eb37dd9525ba666b1cd8
jackcolin21307 <jackcolin21307@gmail.com>
2019-08-05 09:45:34 +0200

    company class, google maps and type definition file for maps added

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import { User } from './User';
+import { User } from "./User";
+import { Company } from "./Company";
 
 const user = new User();
+const company = new Company();
 
-console.log('user', user);
+console.log("user", user);
+console.log("company", company);
+
+google;
